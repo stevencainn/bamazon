7d5f8380f69fa4deb7558ceb7c5ab323c6a08d8f
steven cain <stevencainn@gmail.com>
2019-11-10 17:44:36 -0500

    prompts user and updates inventory when item is chosen

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,11 +21,54 @@ function displayItems(){
     connection.query("SELECT * FROM products", function(err, response){
         if(err) throw err;
         console.table(response);
-        connection.end();
         pickItem();
     });
 }
 
 function pickItem(){
+    inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the item_id you would like to purchase?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?"
+    }]).then(function(answer){
+        
+        var product = answer.item;
+        var quantity = answer.quantity;
+        var userCost;
+        var updateQuantity;
+
+        connection.query("SELECT * FROM products WHERE ?", {item_id: product}, function(err, response){
+            if(err) throw err;
+            console.log(response[0]);
+            if(quantity > response[0].stock_quantity){
+                console.log("Insufficient Quantity!!")
+            }else {
+                userCost = quantity * response[0].price;
+                updateQuantity = response[0].stock_quantity - quantity;
+                console.log(`
+                You have purchased ${quantity} of ${response[0].product_name}
+                Your total is: ${userCost}`);
+            }
+
+            connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: updateQuantity}, {item_id: product}], function(err, response){
+                if (err) throw err;
+                console.log("Inventory Updated")
+            })
+
     
-}
+
+
+
+            connection.end();
+
+        })
+    })
+
+};
